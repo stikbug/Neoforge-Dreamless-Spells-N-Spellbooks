9db8c02adfebd437da2411c0519e2aef3a1e84c6
stikb <stikbuglol@outlook.com>
2025-03-07 19:43:33 -0600

    did stuff

---
+++ src/main/java/net/Iceforkkk/DreamlessAditions/item/ModItems.java
@@ -0,0 +1,19 @@
+package net.Iceforkkk.DreamlessAditions.item;
+
+import net.Iceforkkk.DreamlessAditions.DreamelssSpellsAndSpellbooks;
+import net.minecraft.world.damagesource.DamageEffects;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(DreamelssSpellsAndSpellbooks.MOD_ID);
+
+    public static final DeferredItem<Item> SLEEPLESSCHAIN = ITEMS.register("sleeplesschain",
+            ()-> new Item(new Item.Properties()));
+
+    public static void register (IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
