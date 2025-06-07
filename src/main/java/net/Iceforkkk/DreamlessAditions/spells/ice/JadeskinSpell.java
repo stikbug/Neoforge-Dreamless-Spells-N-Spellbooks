99e4d9cbaa6883f910fb7107ddb67183759f7c5f
stikb <stikbuglol@outlook.com>
2025-06-06 21:24:33 -0500

    Changelog: - Jadeskin spell works!

--- src/main/java/net/Iceforkkk/DreamlessAditions/spells/ice/JadeskinSpell.java
+++ src/main/java/net/Iceforkkk/DreamlessAditions/spells/ice/JadeskinSpell.java
@@ -1,13 +1,11 @@
 package net.Iceforkkk.DreamlessAditions.spells.ice;
 
 import io.redspace.ironsspellbooks.api.config.DefaultConfig;
-import io.redspace.ironsspellbooks.api.events.SpellHealEvent;
 import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
 import io.redspace.ironsspellbooks.api.spells.*;
 import io.redspace.ironsspellbooks.api.util.AnimationHolder;
 import io.redspace.ironsspellbooks.api.util.Utils;
-import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
 import io.redspace.ironsspellbooks.registries.SoundRegistry;
 import net.Iceforkkk.DreamlessAditions.Dreamless_Spells;
 import net.Iceforkkk.DreamlessAditions.effect.ModEffects;
@@ -17,20 +15,19 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.common.NeoForge;
 
 import java.util.List;
 import java.util.Optional;
 
+@AutoSpellConfig
 public class JadeskinSpell extends AbstractSpell {
     private final ResourceLocation spellId = ResourceLocation.fromNamespaceAndPath(Dreamless_Spells.MOD_ID, "jadeskin");
 
     @Override
     public List<MutableComponent> getUniqueInfo(int spellLevel, LivingEntity caster) {
         return List.of(
-                Component.translatable("ui.irons_spellbooks.effect_length", Utils.timeFromTicks(getSpellPower(spellLevel, caster) * 20, 1))
+                Component.translatable("ui.irons_spellbooks.effect_length", Utils.timeFromTicks(getSpellPower(spellLevel, caster) * 40, 1))
         );
     }
 
@@ -72,11 +69,12 @@ public class JadeskinSpell extends AbstractSpell {
 
     @Override
     public void onCast(Level world, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
-        entity.addEffect(new MobEffectInstance(ModEffects.JADESKIN_EFFECT, (int) getSpellPower(spellLevel, entity) * 20, 0, false, false, true));
+        entity.addEffect(new MobEffectInstance(ModEffects.JADESKIN_EFFECT, (int) (getSpellPower(spellLevel, entity) * 40), spellLevel - 1, false, false, true));
         super.onCast(world, spellLevel, entity, castSource, playerMagicData);
     }
     @Override
     public AnimationHolder getCastStartAnimation() {
         return SpellAnimations.SELF_CAST_ANIMATION;
     }
+
 }
